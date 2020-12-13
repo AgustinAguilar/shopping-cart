f39670f6019052aacbc6cc26d5d6ac9f7c772f72
AgustinAguilar <34849138+AgustinAguilar@users.noreply.github.com>
2020-12-13 05:39:18 -0300

    nuevos proyectos para mejora de estructura

---
+++ Supercon.Businnes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Supercon.Businnes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Supercon.DataAccess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Supercon.DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
